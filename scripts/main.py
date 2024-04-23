138890e47370bb54eef71024e103190d60d6874a
Rémi Moreau <rmoreau@albertschool.com>
2024-04-24 00:29:11 +0200

    reparing main.py

--- scripts/main.py
+++ scripts/main.py
@@ -22,40 +22,39 @@ from scraping import download_image
 if __name__=='__main__':
         
     # Scraping
-    # driver = webdriver.Edge()
+    driver = webdriver.Edge()
 
-    # liste_url = [
-    #     "https://www.paruvendu.fr/a/voiture-occasion/peugeot/208/",
-    #     "https://www.paruvendu.fr/a/voiture-occasion/renault/clio/",
-    #     "https://www.paruvendu.fr/a/voiture-occasion/dacia/sandero/",
-    #     "https://www.paruvendu.fr/a/voiture-occasion/citroen/c3/",
-    #     "https://www.paruvendu.fr/a/voiture-occasion/peugeot/2008/",
-    #     "https://www.paruvendu.fr/a/voiture-occasion/renault/captur/",
-    #     "https://www.paruvendu.fr/a/voiture-occasion/peugeot/3008/",
-    #     "https://www.paruvendu.fr/a/voiture-occasion/toyota/yaris/",
-    #     "https://www.paruvendu.fr/a/voiture-occasion/citroen/c4/",
-    #     "https://www.paruvendu.fr/a/voiture-occasion/volkswagen/golf/",
-    #     "https://www.paruvendu.fr/a/voiture-occasion/renault/megane/",
-    #     "https://www.paruvendu.fr/a/voiture-occasion/ford/fiesta/",
-    #     "https://www.paruvendu.fr/a/voiture-occasion/peugeot/308/",
-    #     "https://www.paruvendu.fr/a/voiture-occasion/renault/twingo/",
-    #     "https://www.paruvendu.fr/a/voiture-occasion/volkswagen/polo/"
-    # ]
+    liste_url = [
+        "https://www.paruvendu.fr/a/voiture-occasion/peugeot/208/",
+        "https://www.paruvendu.fr/a/voiture-occasion/renault/clio/",
+        "https://www.paruvendu.fr/a/voiture-occasion/dacia/sandero/",
+        "https://www.paruvendu.fr/a/voiture-occasion/citroen/c3/",
+        "https://www.paruvendu.fr/a/voiture-occasion/peugeot/2008/",
+        "https://www.paruvendu.fr/a/voiture-occasion/renault/captur/",
+        "https://www.paruvendu.fr/a/voiture-occasion/peugeot/3008/",
+        "https://www.paruvendu.fr/a/voiture-occasion/toyota/yaris/",
+        "https://www.paruvendu.fr/a/voiture-occasion/citroen/c4/",
+        "https://www.paruvendu.fr/a/voiture-occasion/volkswagen/golf/",
+        "https://www.paruvendu.fr/a/voiture-occasion/renault/megane/",
+        "https://www.paruvendu.fr/a/voiture-occasion/ford/fiesta/",
+        "https://www.paruvendu.fr/a/voiture-occasion/peugeot/308/",
+        "https://www.paruvendu.fr/a/voiture-occasion/renault/twingo/",
+        "https://www.paruvendu.fr/a/voiture-occasion/volkswagen/polo/"
+    ]
 
-    # nb_model = 1
-    # for url in liste_url:
-    #     driver.get(url)
-    #     liste = pd.DataFrame()
-    #     print("Driver créé")
+    nb_model = 1
+    for url in liste_url:
+        driver.get(url)
+        liste = pd.DataFrame()
+        print("Driver créé")
         
-    #     if nb_model == 1:
-    #         cookie = driver.find_element(By.XPATH, "//button[@onclick='cmp_pv.cookie.saveConsent(true);']")
-    #         cookie.click()
+        if nb_model == 1:
+            cookie = driver.find_element(By.XPATH, "//button[@onclick='cmp_pv.cookie.saveConsent(true);']")
+            cookie.click()
 
-    #     os.makedirs("cars_project", exist_ok=True)
+        os.makedirs("cars_project", exist_ok=True)
 
-    #     extract_image_links(url)
-    #     nb_model+=1
+        extract_image_links(url)
+        nb_model+=1
 
-    # driver.quit()
-    print ('hello')
+    driver.quit()
